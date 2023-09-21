46a77c9b5074eb5018370d6a4515a42313cce2f9
Shaoting-Feng <fengshaoting@sjtu.edu.cn>
2023-09-21 17:09:43 -0500

    updated on 09.22

--- Fairness-main/plot.py
+++ Fairness-main/plot.py
@@ -7,36 +7,9 @@ bottleneck_bw = "10Mbps" # requires changes
 result = ""
 termination_time = 0
 total_bytes_sent = {} # the bytes sent by each flow
+total_bytes_fq = {} # the bytes in fair curve of each flow
 
-'''
-tag_to_max_send_t = {}
-JFI_metric = {}
-'''
-
-'''
-def find_max_time(receive_file, fq_file):
-    global max_time
-    with open(receive_file, 'r') as f:
-        lines = list(reversed(f.readlines()))
-        for line in lines:
-            parts = line.split()
-            if len(parts) >= 5 and parts[1] == 'SourceIDTag:':
-                time = int(parts[0].split('[')[1].strip(']'))
-                if time > max_time:
-                    max_time = time
-            break
-    with open(fq_file, 'r') as f:
-        lines = list(reversed(f.readlines()))
-        for line in lines:
-            parts = line.split()
-            if len(parts) >= 5 and parts[1] == 'SourceIDTag:':
-                time = int(parts[0].split('[')[1].strip(']'))
-                if time > max_time:
-                    max_time = time
-            break
-'''
-
-def process_file(input_file, sent_flag=False):
+def process_file(input_file, sent_flag=False, fq_flag=False):
     tag_to_size_sum = {}
     current_size = {}
     previous_time = {}
@@ -53,6 +26,8 @@ def process_file(input_file, sent_flag=False):
                             tag_to_size_sum[tag][t] = current_size[tag]
                         if sent_flag:
                             total_bytes_sent[tag] = current_size[tag]
+                        elif fq_flag:
+                            total_bytes_fq[tag] = current_size[tag]
                     break
                 else:
                     tag = int(parts[2].strip(','))
@@ -60,6 +35,7 @@ def process_file(input_file, sent_flag=False):
                     if tag not in tag_to_size_sum:
                         current_size[tag] = 0
                         previous_time[tag] = 0
+                        tag_to_size_sum[tag] = np.array([0] * (termination_time + 1))
                     for t in range(previous_time[tag] + 1, time):
                         tag_to_size_sum[tag][t] = current_size[tag]
                     current_size[tag] += size
@@ -71,13 +47,15 @@ def process_file(input_file, sent_flag=False):
                 tag_to_size_sum[tag][t] = current_size[tag]
                 if sent_flag:
                     total_bytes_sent[tag] = current_size[tag]
+                elif fq_flag:
+                    total_bytes_fq[tag] = current_size[tag]
 
     # structure of tag_to_size_sum:
     # tag0: 0 1 1 2 2 2 3
     # tag1: 0 1 3 7 9 9 9
     return tag_to_size_sum 
 
-def plot_tag_data(tag_to_size_sum_sent, tag_to_size_sum_received, tag_to_size_sum_fq, update_lower_headroom, sampling_num):
+def plot_tag_data(tag_to_size_sum_sent, tag_to_size_sum_received, tag_to_size_sum_fq, update_lower_headroom, p_p):
     fig_num = len(tag_to_size_sum_received)
     axs = plt.subplots(nrows=fig_num, ncols=1, figsize=(8, 3*fig_num))[1]
     data_for_idx = [] # to calculate weighted average
@@ -100,11 +78,19 @@ def plot_tag_data(tag_to_size_sum_sent, tag_to_size_sum_received, tag_to_size_su
         axs[tag].set_ylabel("Byte")
         axs[tag].legend()
 
+        bytes_sent = sent[termination_time]
+
+        # Sampling
+        if p_p != {}:
+            fq = fq[p_p[tag]]
+            received = received[p_p[tag]]
+            sent = sent[p_p[tag]]
+
         # Calculation for each flow
         deviation = sum(abs(fq - received))
         upper_head = abs(sent - fq)
         if update_lower_headroom:
-            lower_head = fq - np.concatenate(([0], received[:termination_time-1]))
+            lower_head = fq - np.concatenate(([0], received[:len(received)-1])) # use the received value at the last sampling point as the subtraction 
         else:
             lower_head = fq
         max_head = [max(a, b) for a, b in zip(lower_head, upper_head)]
@@ -114,24 +100,16 @@ def plot_tag_data(tag_to_size_sum_sent, tag_to_size_sum_received, tag_to_size_su
         # Write the result in file
         file_path = "ns/" + result + "/area_idx.txt"
         data_for_idx.append(
-            {"tag": tag, "area_idx": area_idx, "bytes_sent": sent[termination_time]}
+            {"tag": tag, "area_idx": area_idx, "bytes_sent": bytes_sent}
         )
         if i == 0: # first time to write in file
             write_mode = "w"
         else:
             write_mode = "a"
         with open(file_path, write_mode) as file:
-            file.write("The area index for Application " + str(tag) + " is " + str(area_idx) + ".\nApplication " + str(tag) + " has sent " + str(sent[termination_time]) + 
+            file.write("The area index for Application " + str(tag) + " is " + str(area_idx) + ".\nApplication " + str(tag) + " has sent " + str(bytes_sent) + 
                        " bytes.\nThe deviation for Application " + str(tag) + " is " + str(deviation) + ".\nThe max headroom for Application " + str(tag) + " is " + 
                        str(max_headroom) + ".\n")
-            
-        '''
-        # added on 08.22 to calculate JFI metric for each app
-        # throughput for JFI is calculated when the sending behavior is terminated
-        JFI_metric[tag1] = cumulative_sum2[tag_to_max_send_t[tag1]] / cumulative_sum1[tag_to_max_send_t[tag1]]
-        with open(file_path, "a") as file:
-            file.write("Normalised throughput of Application " + str(tag1) + " in the sending period is " + str(JFI_metric[tag1]) + ".\n")
-        '''
 
     # Weigh area_idx for each flow based on total bytes sent
     WAI_numerator = 0
@@ -142,28 +120,6 @@ def plot_tag_data(tag_to_size_sum_sent, tag_to_size_sum_received, tag_to_size_su
     WAI = WAI_numerator / WAI_denominator
     with open(file_path, "a") as file:
         file.write("WAI is " + str(WAI) + ".\n")
-
-    '''
-    # added on 08.31 to calculate normalised area index
-    NAI_numerator = 0
-    NAI_denominator = 0
-    for data in data_for_idx:
-        NAI_numerator = NAI_numerator + data["deviation"]
-        NAI_denominator = NAI_denominator + data["max_headroom"]
-    NAI = NAI_numerator / NAI_denominator
-    with open(file_path, "a") as file:
-        file.write("NAI is " + str(NAI) + ".\n")
-    '''
-
-    '''
-    # added on 08.22 to calculate JFI
-    N = len(JFI_metric)
-    sum_xi = sum(JFI_metric.values())
-    sum_xi_squared = sum(x**2 for x in JFI_metric.values())
-    JFI = (sum_xi**2) / (N * sum_xi_squared)
-    with open(file_path, "a") as file:
-        file.write("JFI is " + str(JFI) + ".\n")
-    '''
 
     plt.tight_layout()
     plt.savefig("ns/" + result + "/output_chart.svg", format="svg")
@@ -179,12 +135,29 @@ def parse_args():
     return parser.parse_args()
 
 def sample(tag_to_size_sum_fq, sampling_num):
-    point_num = {}
-
+    point_position = {}
     total = sum(total_bytes_sent.values())
     for i, (tag, bytes) in enumerate(total_bytes_sent.items()):
-        point_num[tag] = (sampling_num * bytes) // total
-        
+        point_num = int(round((sampling_num*bytes)/total, 0)) # sum of point_num may not equal sampling_num
+        threshold = []
+        for j in range(1, point_num + 1):
+            threshold.append(total_bytes_fq[tag]/j)
+        th_count = 0
+        point_position[tag] = np.array([0] * point_num)
+        for j in range(termination_time+1):
+            if tag_to_size_sum_fq[tag][j] >= threshold[th_count]:
+                point_position[tag][th_count] = j
+                th_count = th_count + 1
+                if th_count >= point_num:
+                    break    
+        '''test'''
+        print(tag)
+        print(threshold)
+        '''end test''' 
+    '''test'''
+    print(point_position)
+    '''end test''' 
+    return point_position 
 
 def main():
     global result
@@ -200,12 +173,14 @@ def main():
 
     tag_to_size_sum_received = process_file("ns/" + result + "/fifo/received_ms.dat")
     tag_to_size_sum_sent = process_file("ns/" + result + "/fifo/sent_ms.dat", True)
-    tag_to_size_sum_fq = process_file("ns/" + result + "/fq/received_ms.dat")
+    tag_to_size_sum_fq = process_file("ns/" + result + "/fq/received_ms.dat", False, True)
 
     if sampling_num != 0:
-        sample(tag_to_size_sum_fq, sampling_num)
+        p_p = sample(tag_to_size_sum_fq, sampling_num)
+    else:
+        p_p = {}
 
-    plot_tag_data(tag_to_size_sum_sent, tag_to_size_sum_received, tag_to_size_sum_fq, upd_LHR, sampling_num)
+    plot_tag_data(tag_to_size_sum_sent, tag_to_size_sum_received, tag_to_size_sum_fq, upd_LHR, p_p)
 
 if __name__ == "__main__":
     main()
